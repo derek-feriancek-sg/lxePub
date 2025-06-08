303bf62887933ef4d5b910b07ea351a9e7228a53
Amp <amp@ampcode.com>
2025-06-07 18:28:48 -0700

    Add pixel art house to html game
    https://ampcode.com/threads/T-749282b4-4e74-4f82-a822-339b23db62d8

--- game.js
+++ game.js
@@ -12,6 +12,13 @@ const player = {
     color: '#e74c3c'
 };
 
+const house = {
+    x: 100,
+    y: 200,
+    width: 120,
+    height: 120
+};
+
 const keys = {
     ArrowUp: false,
     ArrowDown: false,
@@ -38,6 +45,30 @@ function drawPlayer() {
     drawPixelRect(player.x + 20, player.y + 20, 8, 12, '#3498db');
 }
 
+function drawHouse() {
+    // Main house body
+    drawPixelRect(house.x, house.y + 40, house.width, 80, '#8b4513');
+    
+    // Roof
+    drawPixelRect(house.x - 8, house.y + 32, house.width + 16, 16, '#e74c3c');
+    drawPixelRect(house.x + 8, house.y + 16, house.width - 16, 16, '#e74c3c');
+    drawPixelRect(house.x + 24, house.y, house.width - 48, 16, '#e74c3c');
+    
+    // Door
+    drawPixelRect(house.x + 45, house.y + 80, 16, 40, '#34495e');
+    drawPixelRect(house.x + 56, house.y + 95, 2, 2, '#f39c12');
+    
+    // Windows
+    drawPixelRect(house.x + 20, house.y + 60, 16, 16, '#3498db');
+    drawPixelRect(house.x + 84, house.y + 60, 16, 16, '#3498db');
+    
+    // Window frames
+    drawPixelRect(house.x + 26, house.y + 60, 4, 16, '#34495e');
+    drawPixelRect(house.x + 20, house.y + 66, 16, 4, '#34495e');
+    drawPixelRect(house.x + 90, house.y + 60, 4, 16, '#34495e');
+    drawPixelRect(house.x + 84, house.y + 66, 16, 4, '#34495e');
+}
+
 function updatePlayer() {
     if (keys.ArrowUp && player.y > 0) {
         player.y -= player.speed;
@@ -56,6 +87,7 @@ function updatePlayer() {
 function gameLoop() {
     ctx.clearRect(0, 0, canvas.width, canvas.height);
     
+    drawHouse();
     updatePlayer();
     drawPlayer();
     
